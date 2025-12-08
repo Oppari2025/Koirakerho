085339d1f9976edafb38a26b2cd9b75be57c4ffe
Joonatan N <112549793+JoonatanNiinimaa@users.noreply.github.com>
2025-12-08 10:14:12 +0200

    Updated teamcard
    -Teams are now clickable on profile

--- components/teamCard.tsx
+++ components/teamCard.tsx
@@ -1,7 +1,7 @@
 import { Card } from '@/components/ui/card';
 import { Heading } from '@/components/ui/heading';
 import React from 'react';
-import { Image, ScrollView, View } from 'react-native';
+import { Image, ScrollView, TouchableOpacity, View } from 'react-native';
 
 export default function DogCard({ name, image }: { name: string; image: any }) {
 
@@ -9,7 +9,7 @@ export default function DogCard({ name, image }: { name: string; image: any }) {
     const Data = {
         Team1: { name: 'Koira kuiskaajat', image: require('@/assets/images/Team1.jpg') },
         Team2: { name: 'Ulkoiluttajat', image: require('@/assets/images/Team2.jpg') },
-        };
+    };
 
 
     return (
@@ -17,10 +17,13 @@ export default function DogCard({ name, image }: { name: string; image: any }) {
             <ScrollView horizontal showsHorizontalScrollIndicator={false} className="mt-4">
                 {Object.values(Data).map((team, index) => (
                     <Card key={index} size="md" variant="elevated" className="m-3 bg-gray-600 p-2 rounded-lg">
-                        <Image source={team.image} className="h-32 w-32 rounded-lg mb-2" />
-                        <Heading size="lg" className="mb-1 text-white self-center">
-                            {team.name}
-                        </Heading>
+                        {/* <TouchableOpacity key={index} onPress={() => router.push(`/teamProfileScreen`)}> */}
+                        <TouchableOpacity key={index} onPress={() => console.log('Team pressed')}>
+                            <Image source={team.image} className="h-32 w-32 rounded-lg mb-2" />
+                            <Heading size="lg" className="mb-1 text-white self-center">
+                                {team.name}
+                            </Heading>
+                        </TouchableOpacity>
                     </Card>
                 ))}
             </ScrollView>
