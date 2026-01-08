aff6adb1fdce9d00d0dfcead38481149f15fc3b3
Joonatan N <112549793+JoonatanNiinimaa@users.noreply.github.com>
2026-01-08 11:25:26 +0200

    Lisäys
    Lisätty perus valinnat

--- app/(main)/addDogScreen.tsx
+++ app/(main)/addDogScreen.tsx
@@ -1,21 +1,103 @@
-import { Card } from '@/components/ui/card';
-import { Heading } from '@/components/ui/heading';
-import React from 'react';
-import { Image, View } from 'react-native';
+import { FormControl, FormControlLabel, FormControlLabelText } from '@/components/ui/form-control';
+import { CircleIcon } from '@/components/ui/icon';
+import { Radio, RadioGroup, RadioIcon, RadioIndicator, RadioLabel } from '@/components/ui/radio';
+import { Textarea } from '@/components/ui/textarea';
+import { VStack } from '@/components/ui/vstack';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function addDogScreen() {
+    const [values, setValues] = React.useState('Male');
+    const [size, setSize] = React.useState('Small');
+    const [descHeight, setDescHeight] = useState(0);
+
 
     return (
         <SafeAreaView className="flex-1 px-4 pt-6">
-            <View className="m-6 p-5 bg-white rounded-lg">
-                    <Card size="lg" variant="elevated" className="m-3 bg-gray-600 p-2 rounded-lg">
-                        <Image source={require('@/assets/images/dog1.jpg')} className="h-64 w-64 rounded-lg mb-2" />
-                        <Heading size="lg" className="mb-1 text-white self-center">
-                            TÄMÄ ON ADDDOG
-                        </Heading>
-                    </Card>
+            <View className="flex-row mb-4">
+                <FormControl className="flex-1">
+                    <FormControlLabel>
+                        <FormControlLabelText>Name</FormControlLabelText>
+                    </FormControlLabel>
+                    <Textarea className="h-12 w-4/5 bg-white  rounded-lg">
+                        <TextInput />
+                    </Textarea>
+                </FormControl>
+                <FormControl className="flex-1">
+                    <FormControlLabel>
+                        <FormControlLabelText>Age</FormControlLabelText>
+                    </FormControlLabel>
+                    <Textarea className="h-12 w-20 bg-white  rounded-lg">
+                        <TextInput />
+                    </Textarea>
+                </FormControl>
             </View>
+            <FormControl>
+                <FormControlLabel>
+                    <FormControlLabelText>Description</FormControlLabelText>
+                </FormControlLabel>
+                <Textarea className="h-20  bg-white  rounded-lg">
+                    <TextInput placeholder='How would you describe your dog to others?'/>
+                </Textarea>
+            </FormControl>
+            <FormControl>
+                <FormControlLabel>
+                    <FormControlLabelText>Breed</FormControlLabelText>
+                </FormControlLabel>
+                <Textarea className="h-12 w-3/6 bg-white  rounded-lg">
+                    <TextInput />
+                </Textarea>
+            </FormControl>
+            <FormControl>
+                <FormControlLabel>
+                    <FormControlLabelText>Male or female</FormControlLabelText>
+                </FormControlLabel>
+                <RadioGroup className="my-2" value={values} onChange={setValues}>
+                    <VStack space="sm">
+                        <Radio size="sm" value="Mango">
+                            <RadioIndicator>
+                                <RadioIcon as={CircleIcon} />
+                            </RadioIndicator>
+                            <RadioLabel>Male</RadioLabel>
+                        </Radio>
+                        <Radio size="sm" value="Apple">
+                            <RadioIndicator>
+                                <RadioIcon as={CircleIcon} />
+                            </RadioIndicator>
+                            <RadioLabel>Female</RadioLabel>
+                        </Radio>
+                    </VStack>
+                </RadioGroup>
+                <FormControl>
+                    <FormControlLabel>
+                        <FormControlLabelText>Size</FormControlLabelText>
+                    </FormControlLabel>
+                    <RadioGroup className="my-2" value={size} onChange={setSize}>
+                        <VStack space="sm">
+                            <Radio size="sm" value="Small">
+                                <RadioIndicator>
+                                    <RadioIcon as={CircleIcon} />
+                                </RadioIndicator>
+                                <RadioLabel>Small</RadioLabel>
+                            </Radio>
+                            <Radio size="sm" value="Medium">
+                                <RadioIndicator>
+                                    <RadioIcon as={CircleIcon} />
+                                </RadioIndicator>
+                                <RadioLabel>Medium</RadioLabel>
+                            </Radio>
+                            <Radio size="sm" value="Large">
+                                <RadioIndicator>
+                                    <RadioIcon as={CircleIcon} />
+                                </RadioIndicator>
+                                <RadioLabel>Large</RadioLabel>
+                            </Radio>
+                        </VStack>
+                    </RadioGroup>
+                </FormControl>
+            </FormControl>
         </SafeAreaView>
     );
 }
