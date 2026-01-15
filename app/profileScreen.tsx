584548c962a8873fb29bae57cf1d3c28e9dda662
Joonatan N <112549793+JoonatanNiinimaa@users.noreply.github.com>
2026-01-15 12:07:12 +0200

    - Päivittää profiilissa koiralistan

--- app/profileScreen.tsx
+++ app/profileScreen.tsx
@@ -6,14 +6,34 @@ import { HStack } from '@/components/ui/hstack';
 import { Text } from '@/components/ui/text';
 import { VStack } from '@/components/ui/vstack';
 import { useAuth } from '@/src/context/AuthContext';
-import React from 'react';
+import { getMyDogs } from '@/src/services/dogService';
+import { useFocusEffect } from 'expo-router';
+import React, { useCallback, useState } from 'react';
 import { View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function profileScreen() {
     const { userProfile } = useAuth()
+    const [dog, setDogs] = useState<any[]>([])
+    const [status, setStatus] = useState<string | null>(null)
     
+        const loadDogs = async () => {
+            setStatus('Ladataan koiria...')
+            try {
+                const list = await getMyDogs()
+                setDogs(list)
+                setStatus(null)
+            } catch (e: any) {
+                console.error(e)
+                setStatus(`Koirien lataus epäonnistui: ${e?.message ?? e}`)
+            }
+        }
 
+          useFocusEffect(
+    useCallback(() => {
+      loadDogs();
+    }, [])
+  );
     return (
         <SafeAreaView className="flex-1 px-4 pt-6">
             <View className="m-6 p-5 bg-white rounded-lg">
@@ -32,7 +52,7 @@ export default function profileScreen() {
             </View>
             <View className="mt-4">
                 <Text className="text-2xl font-bold text-white">My Dogs</Text>
-                <DogCard name={''} image={undefined} />
+                <DogCard dog={dog} name={''} image={undefined} />
             </View>
             <View className="mt-4">
                 <Text className="text-2xl font-bold text-white">My Teams</Text>
--- components/dogCard.tsx
+++ components/dogCard.tsx
@@ -1,44 +1,20 @@
 import { Card } from '@/components/ui/card';
 import { Heading } from '@/components/ui/heading';
-import { getMyDogs } from '@/src/services/dogService';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { router } from 'expo-router';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Image, ScrollView, TouchableOpacity, View } from 'react-native';
 
-export default function DogCard({ name, image }: { name: string; image: any }) {
-
-    const [status, setStatus] = useState<string | null>(null)
-    const [dogs, setDogs] = useState<any[]>([])
-
-    useEffect(() => {
-        loadDogs()
-    }, [])
-
-    const loadDogs = async () => {
-        setStatus('Ladataan koiria...')
-        try {
-            const list = await getMyDogs()
-            setDogs(list)
-            setStatus(null)
-        } catch (e: any) {
-            console.error(e)
-            setStatus(`Koirien lataus epäonnistui: ${e?.message ?? e}`)
-        }
-    }
-
-    const handleDogInfo = async () => {
-        console.log(dogs.map(dog => ({ name: dog.name, imageUrl: dog.imageUrl, id: dog.id, breed: dog.breed, age: dog.age, description: dog.description, gender: dog.gender, size: dog.size })))
-    }
+export default function DogCard({dog, name, image }: { dog: any[]; name: string; image: any }) {
 
     return (
         <View>
             <ScrollView horizontal showsHorizontalScrollIndicator={false} className="mt-4">
-                {dogs.map(dog => (
+                {dog.map(dog => (
                     <Card key={dog.id} size="md" variant="elevated" className="m-3 bg-gray-600 p-2 rounded-lg">
                         <TouchableOpacity
                             onPress={() =>
-                                router.push({
+                                router.replace({
                                     pathname: '/dogProfileScreen',
                                     params: {
                                         id: dog.id,
