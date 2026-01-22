fbf3adbae649fee0ef663ce79af278cfd1eed410
MiikkaJanesaro <miikka.janesaro@gmail.com>
2026-01-22 15:29:48 +0200

    group

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,12 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Drawer } from 'expo-router/drawer';
+import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
-import { AuthProvider } from '../src/context/AuthContext';
+import { AuthProvider, useAuth } from '@/src/context/AuthContext';
+
 
 import '@/global.css';
 import { ActivityIndicator, View } from 'react-native';
@@ -13,19 +14,7 @@ import { ActivityIndicator, View } from 'react-native';
 export default function RootLayout() {
   return (
     <AuthProvider>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Drawer>
-          <Drawer.Screen
-            name="(main)"
-            options={{ drawerLabel: 'Home', title: 'Home', headerShown: false }}
-          />
-          <Drawer.Screen
-            name="profileScreen"
-            options={{ drawerLabel: 'My Profile', title: 'My Profile', headerShown: false }}
-          />
-        </Drawer>
-        <StatusBar style="auto" />
-      </ThemeProvider>
+      <InnerRoot />
     </AuthProvider>
   );
 }
